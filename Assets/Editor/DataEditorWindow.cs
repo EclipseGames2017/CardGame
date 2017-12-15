c55e731f56d7c7972f782eb6e6e05c9365216eec
Jamie M Sams <j.sams@uos.ac.uk>
2017-12-15 23:24:59 +0000

    Created a blank editor window for the card editor tool

---
+++ Assets/Editor/DataEditorWindow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace EclipseStudios.CardGame
+{
+    public class DataEditorWindow : EditorWindow
+    {
+        public DataEditorWindow()
+        {
+            this.titleContent = new GUIContent("Card Editor");
+        }
+
+        [MenuItem("Tools/CardEditor")]
+        static void Init()
+        {
+            DataEditorWindow dew = EditorWindow.GetWindow<DataEditorWindow>();
+            dew.Show();
+        }
+
+        void OnGUI()
+        {
+            GUILayout.Label("Hello, World!");
+        }
+    }
+}
