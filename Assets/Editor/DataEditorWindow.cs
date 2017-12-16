e0fb6e1763893e0515b71319262f4be17c52219c
Jamie M Sams <j.sams@uos.ac.uk>
2017-12-16 19:32:32 +0000

    Did some work on the editor window for editing the card data file

--- Assets/Editor/DataEditorWindow.cs
+++ Assets/Editor/DataEditorWindow.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEditor;
 
@@ -7,6 +8,13 @@ namespace EclipseStudios.CardGame
 {
     public class DataEditorWindow : EditorWindow
     {
+        const string dataPath = "Assets/Data/card_data.json";
+        CardList cards;
+
+        Vector2 scrollViewPosition = Vector2.zero;
+
+        List<bool> foldoutStates;
+
         public DataEditorWindow()
         {
             this.titleContent = new GUIContent("Card Editor");
@@ -21,7 +29,78 @@ namespace EclipseStudios.CardGame
 
         void OnGUI()
         {
-            GUILayout.Label("Hello, World!");
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("Load Card Data"))
+            {
+                LoadCardData();
+            }
+            if (GUILayout.Button("Save Card Data"))
+            {
+                SaveCardData();
+            }
+            GUILayout.EndHorizontal();
+
+            if (cards == null)
+            {
+                return;
+            }
+
+            ChangeArraySize(EditorGUILayout.IntField("Card Count", cards.Length));
+
+            scrollViewPosition = GUILayout.BeginScrollView(scrollViewPosition);
+            for (int i = 0; i < cards.Length; i++)
+            {
+                string title = (cards[i].Name_En == "") ? "Card " + i : cards[i].Name_En;
+                foldoutStates[i] = EditorGUILayout.Foldout(foldoutStates[i], title, true);
+                if (foldoutStates[i])
+                {
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Space(30);
+                    cards[i].Name_En = EditorGUILayout.TextField("Name (English)", cards[i].Name_En);
+                    GUILayout.EndHorizontal();
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Space(30);
+                    cards[i].Name = EditorGUILayout.TextField("Name", cards[i].Name);
+                    GUILayout.EndHorizontal();
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Space(30);
+                    cards[i].Occupation = EditorGUILayout.TextField("Occupation", cards[i].Occupation);
+                    GUILayout.EndHorizontal();
+                }
+            }
+            GUILayout.EndScrollView();
+        }
+
+        void ChangeArraySize(int newSize)
+        {
+            // TODO: If the new size is larger, just resize it
+            // TODO: If the new size is smaller, show a warning that data will be lost
+        }
+
+        void LoadCardData()
+        {
+            using (StreamReader sr = new StreamReader(dataPath))
+            {
+                string json = sr.ReadToEnd();
+                cards = JsonUtility.FromJson<CardList>(json);
+            }
+
+            foldoutStates = new List<bool>();
+            for (int i = 0; i < cards.Length; i++)
+            {
+                foldoutStates.Add(false);
+            }
+        }
+
+        void SaveCardData()
+        {
+            using (StreamWriter sw = new StreamWriter(dataPath))
+            {
+                string json = JsonUtility.ToJson(cards);
+                sw.Write(json);
+            }
+
+            AssetDatabase.ImportAsset(dataPath, ImportAssetOptions.ForceUpdate);
         }
     }
 }
---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EclipseStudios.CardGame
+{
+    [System.Serializable]
+    public class Card
+    {
+        public string Name;
+        public string Name_En;
+        public string Occupation;
+    }
+
+    [System.Serializable]
+    public class CardList
+    {
+        public Card[] cards;
+
+        public CardList(Card[] cards)
+        {
+            this.cards = cards;
+        }
+
+        public Card this[int index]
+        {
+            get
+            {
+                return cards[index];
+            }
+            set
+            {
+                cards[index] = value;
+            }
+        }
+
+        public int Length
+        {
+            get
+            {
+                return cards.Length;
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return cards.GetEnumerator();
+        }
+    }
+}
